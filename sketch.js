9b7c48a2668346ff2bb469e4493b77878dd87cbe
JaineelB <84726291+JaineelB@users.noreply.github.com>
2021-06-10 19:41:56 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -17,13 +17,13 @@ function draw() {
  car.collide(wall)
    
   
-  if (deformation<100 && car.isTouching(wall))  {
+  if (deformation<100)  {
     car.shapeColor = color(0,255,0);
   }
-  if (deformation>100 && deformation<180 && car.isTouching(wall)){
+  if (deformation>100){
     car.shapeColor =color(230,230,0);
   }
-  if(deformation>180 && car.isTouching(wall)){
+  if(deformation>180){
     car.shapeColor =color(255,0,0);
   }
   drawSprites();
